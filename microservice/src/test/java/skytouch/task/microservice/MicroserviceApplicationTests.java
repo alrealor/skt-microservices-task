e9d8e24353dc9eb2ac87d85bd59720675bce6202
Alan Altamirano <alan.altamirano@EDCMXENLT1146.local>
2023-03-14 16:22:18 -0600

    initial scaffolding

---
+++ microservice/src/test/java/skytouch/task/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package skytouch.task.microservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2ed0c3d337a20d235ec39b63a5ec23a0dc1e2be4
Alan Altamirano <alan.altamirano@EDCMXENLT1146.local>
2023-03-28 14:28:20 -0600

    initial commit

--- common-lib/src/main/java/com/skt/task/common/constants/ErrorCodes.java
+++
@@ -1,8 +0,0 @@
-package com.skt.task.common.constants;
-
-public class ErrorCodes {
-    public static final String PRODUCT_NOT_CREATED = "BSN001";
-    public static final String INVALID_PRODUCT_NAME= "IME001";
-    public static final String INVALID_PRODUCT_PRICE = "IME002";
-    public static final String STANDARD_CONNECTION_CODE = "STDCNT";
-}

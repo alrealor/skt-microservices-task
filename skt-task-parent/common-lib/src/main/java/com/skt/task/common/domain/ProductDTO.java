0286a11b2d9f847620296ec7c3c17186fcc8bdf1
Alan Altamirano <alan.altamirano@EDCMXENLT1146.local>
2023-03-29 12:07:38 -0600

    fixing product dto bean validations

--- skt-task-parent/common-lib/src/main/java/com/skt/task/common/domain/ProductDTO.java
+++ skt-task-parent/common-lib/src/main/java/com/skt/task/common/domain/ProductDTO.java
@@ -5,7 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 /**
@@ -18,9 +21,9 @@ public class ProductDTO {
     @NotBlank
     @Size(max = 150)
     private String name;
+
     @DecimalMin(value = "1.00")
-    @DecimalMax(value = "999999999999.99")
-    @Digits(integer = 12, fraction = 2)
-    @Min(1)
+    @DecimalMax(value = "9999999999.99")
+    @NotNull
     private BigDecimal price;
 }
